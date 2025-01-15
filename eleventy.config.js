627d887a8429227216f3c16f833d406d1e01e82c
adamdjbrett <adamdjbrett@users.noreply.github.com>
2025-01-15 14:05:28 -0600

    admin update
    moving /admin back to /public

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,8 +17,6 @@ export default async function(eleventyConfig) {
 
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
-	eleventyConfig
-		.addPassthroughCopy("./admin/");
 		.addPassthroughCopy({
 			"./public/": "/"
 		})
