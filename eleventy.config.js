44f515a09d5981131b25426bfb14e0a81cb2473c
adamdjbrett <adamdjbrett@users.noreply.github.com>
2025-01-15 14:03:16 -0600

    Update eleventy.config.js
    add admin to passthrough

--- eleventy.config.js
+++ eleventy.config.js
@@ -18,6 +18,7 @@ export default async function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig
+		.addPassthroughCopy("./admin/");
 		.addPassthroughCopy({
 			"./public/": "/"
 		})
